f430ba4ce171d9732d716a7a124455f57e168bf7
imanudin-it <imanudin-it@gmail.com>
2024-09-25 13:29:54 +0700

    add

--- pegawai/pages/akun.php
+++ pegawai/pages/akun.php
@@ -1,10 +1,10 @@
 
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<div class="card mb-4">
+<div class="card mb-2">
 <h5 class="card-header mb-3"> <i class="bx bx-list-ol"></i> Pengaturan Akun :
 <span style="float:right" ><small><a href="javascript: window.history.back()"class="badge badge-sm bg-warning">&laquo; Kembali</a></small></span> </h5>
-     <div class="card-body mb-2">
-     <div class="card-body p-0">
+</div>  
+
                       <!-- <div class="d-flex align-items-start align-items-sm-center gap-4">
                         <img src="../assets/img/avatars/user.png" alt="user-avatar" class="d-block rounded" height="100" width="100" id="uploadedAvatar">
                        <div><h3><?=$_SESSION['data']['nama'];?> </h3> </div>
@@ -12,17 +12,18 @@
                         </div> -->
                         <div class="row">
                             <div class="col-md-4 col-lg-4">
-                             
+                            <div class="card mb-4">
+                            <div class="card-body mb-2">
                             <div class="d-flex align-items-start align-items-sm-center gap-4 mb-4">
                         <img src="../assets/img/avatars/user.webp" alt="user-avatar" class="d-block rounded" height="100" width="100" id="uploadedAvatar">
                         <div class="button-wrapper">
                         <h5> <?=$_SESSION['data']['nama'];?> </h5>
                         <p class="text-muted mb-0"><?=$_SESSION['data']['nip'];?></p>
                         </div>
-                        
+                            </div>
                       </div>
-                      
-
+                            </div>
+                            </div>
 
                         <!-- <div class="d-flex justify-content-center pt-4 gap-2">
                             <div class="flex-shrink-0" style="position: relative;">
@@ -35,7 +36,6 @@
                               <small class="text-muted"> <?=$_SESSION['data']['nip'];?></small>
                             </div>
                           </div> -->
-                            </div>
                             <div class="col-md-8 col-lg-8">
                                 <div class="card">
                                 <h5 class="card-header mb-0"> Ganti Password :</h5>
@@ -65,8 +65,7 @@
                                 
                             </div>
                     </div>
-  </div>
-</div>
+
 <script>
         function togglePasswordVisibility() {
             var currentPassword = document.getElementById("current_password");
